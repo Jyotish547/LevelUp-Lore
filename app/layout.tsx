fb0fd69b5384709f05c7ba6662951fdafdc6c24e
Jyotish547 <jyotish547@gmail.com>
2024-04-29 15:32:43 -0600

    speedInsights Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <body className={lato.className}>
         {/* Layout UI */}
         {children}
